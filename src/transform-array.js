d819f2463a85a857773dc9d0ecfb1eafdb68974b
leealexeyvl <leealexey_94@mail.ru>
2020-10-04 18:11:11 +0300

    feat: add transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Not array');
+  }
+
+  let result = arr.slice();
+
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] == '--discard-next') {
+      result[i] = undefined;
+      result[i + 1] = undefined;
+      i += 1;
+    } else if (result[i] == '--discard-prev') {
+      result[i] = undefined;
+      result[i - 1] = undefined;
+    } else if (result[i] == '--double-next') {
+      result[i] = result[i + 1];
+    } else if (result[i] == '--double-prev') {
+      result[i] = result[i - 1];
+    } 
+  }
+
+    return result.filter(item => item != undefined);
+
 };
